7d69695c4645297eb9ae9c15bd302661f8aed561
laluiz <luiz280530@hotmail.com>
2021-05-14 20:39:57 -0300

    React branch-react

---
+++ src/components/Counter/index.tsx
@@ -0,0 +1,18 @@
+import React, {useState} from 'react';
+
+export function Counter(){
+
+    const  [counter, setCounter] = useState(0);
+
+    function handleClick(){
+        setCounter(counter + 1)
+
+    }
+
+    return (
+        <button onClick={handleClick}>Contador {counter}</button>
+    )
+
+
+
+}
